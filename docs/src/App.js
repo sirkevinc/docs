cbb9414d0b415244c088303da4e68f5fb3fa5231
sirkevinc <16568166+sirkevinc@users.noreply.github.com>
2019-05-28 16:54:48 -0400

    Initial Commit

---
+++ docs/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+})
+
+// // Define our app...
+// class App extends React.Component {
+//   // Set the initial value when the app is first constructed.
+//   state = {
+//     value: initialValue,
+//   }
+
+//   // On change, update the app's React state with the new editor value.
+//   onChange = ({ value }) => {
+//     this.setState({ value })
+//   }
+
+//   // Render the editor.
+//   render() {
+//     return <Editor value={this.state.value} onChange={this.onChange} />
+//   }
+// }
+
+const App = () => {
+  const [value, setValue] = useState(initialValue);
+
+  return <Editor value={value} onChange={opts => setValue(opts.value)} />
+}
+
+export default App;
